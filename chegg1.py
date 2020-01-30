73b86a38d025a01bde0f15c132eefff063fdcca1
raghu2 <raghubharadwaj2010@gmail.com>
2020-01-30 23:17:44 +0530

    files assign

---
+++ chegg1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as Tk
+x1=[-2.43061002,0.90894673,0.44822824,4.49246756,3.37311701]
+x2=[-3.67539466,-0.821616,0.08635932,-4.08418949,-0.95860355]
+y=[1,0,1,1,0]
+plt.scatter(x1,x2)
+plt.show()
